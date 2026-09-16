db2bc7e8272c46037efe9dd94afeeb2840820db7
ashwinirajn <ashwinioffice25@gmail.com>
2026-09-17 01:20:15 +0300

    fixed the formt in backend file

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "tfstrgacc"
     container_name       = "tfstate"
     key                  = "tf-module-deploy-azure.tfstate"
-    use_azuread_auth = true
+    use_azuread_auth     = true
   }
 }
