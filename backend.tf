e9d1a85c1da14307863a31d2a727c4e108ec73d7
ashwinirajn <ashwinioffice25@gmail.com>
2026-09-17 01:12:13 +0300

    first devops pipeline

--- backend.tf
+++ backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "tfstrgacc"
     container_name       = "tfstate"
     key                  = "tf-module-deploy-azure.tfstate"
+    use_azuread_auth = true
   }
 }
